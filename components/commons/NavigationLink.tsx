48156473d1d6fbae9c73ae648ba8f0213eac344c
Furkan Can Zirek <62845827+furkancanzirek@users.noreply.github.com>
2024-02-27 14:28:15 +0300

    fix: navigation link active class

--- components/commons/NavigationLink.tsx
+++ components/commons/NavigationLink.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Link from "next/link";
 import { AtSignIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 interface NavigationLinkProps {
   href: string;
@@ -15,25 +18,30 @@ export const NavigationLink: React.FC<NavigationLinkProps> = ({
   children,
   className,
   label,
-    icon,
+  icon,
 }) => {
-  const isActive = false;
-  const iconCmp = icon ?? <AtSignIcon size={16} />
+  const pathname = usePathname();
+  //make active if the current path is the same as the href or the href is a substring of the current path starts with
+  const isActive =
+    pathname === href || (pathname.startsWith(href) && href !== "/");
+  const iconCmp = icon ?? <AtSignIcon size={16} />;
   return (
-    <Link
-      key={href}
-      href={href}
-      className={cn(
-        "group flex items-center justify-between rounded-lg p-2",
-        isActive ? "bg-black text-white" : "hover:bg-gray-200"
-      )}
-    >
-      <span className="flex items-center gap-2">
+    <>
+      <Link
+        key={href}
+        href={href}
+        className={cn(
+          "group flex items-center justify-between rounded-lg py-2 px-3.5 ",
+          isActive ? "bg-black text-white" : "hover:bg-gray-200"
+        )}
+      >
+        <span className="flex items-center gap-2">
           {iconCmp}
-        <span className={cn("font-medium", isActive && "text-white")}>
-          {label}
+          <span className={cn("font-medium", isActive && "text-white")}>
+            {label}
+          </span>
         </span>
-      </span>
-    </Link>
+      </Link>
+    </>
   );
 };

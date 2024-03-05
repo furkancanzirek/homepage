e34e9973a8e345e12df5ceebcdb93162f9a6ba71
Furkan Can Zirek <62845827+furkancanzirek@users.noreply.github.com>
2024-03-05 19:21:01 +0300

    fix: menu list sorted newest to oldest

--- components/MenuList.tsx
+++ components/MenuList.tsx
@@ -11,11 +11,14 @@ interface Write {
 
 const MenuList = async () => {
   const writes = await getFilesWithCount(10);
-  const groupedWrites: { [key: string]: Write[] } = {};
-
+  let groupedWrites: { [key: string]: Write[] } = {};
   writes.forEach((write) => {
     const year = dateToYear(new Date(write.date));
-    groupedWrites[year] = [...(groupedWrites[year] || []), write];
+    if (!groupedWrites[year]) {
+      groupedWrites[year] = [];
+    }
+
+    groupedWrites[year].push(write);
   });
 
   return (
@@ -26,44 +29,48 @@ const MenuList = async () => {
         <span className="col-span-4 hidden text-left md:grid">Title</span>
       </div>
       <div className="group/wrapper">
-        {Object.entries(groupedWrites).map(([year, writes], yearIndex) => (
-          <div key={year} className="mb-4 group/col">
-            <ul className="group/item">
-              {writes.map((write, writeIndex) => (
-                <Link
-                  key={write.slug}
-                  href={`/writings/${write.slug}`}
-                  className=""
-                >
-                  <li
+        {Object.entries(groupedWrites)
+          .sort(([yearA], [yearB]) => Number(yearB) - Number(yearA))
+          .map(([year, writes], yearIndex) => (
+            <div key={year} className="mb-4 group/col">
+              <ul className="group/item">
+                {writes.map((write, writeIndex) => (
+                  <Link
                     key={write.slug}
-                    className="grid grid-cols-6 pl-0  group-hover/wrapper:text-gray-300 
+                    href={`/writings/${write.slug}`}
+                    className=""
+                  >
+                    <li
+                      key={write.slug}
+                      className="grid grid-cols-6 pl-0  group-hover/wrapper:text-gray-300 
                   transition-all duration-100
                     hover:!text-gray-900
                   "
-                  >
-                    <span
-                      className={cn(
-                        "col-span-1 py-4 group-hover/item:text-gray-900 transition-all",
-                        writeIndex === 0 ? "border-t" : ""
-                      )}
                     >
-                      {writeIndex === 0 ? dateToYear(new Date(write.date)) : ""}
-                    </span>
-                    <span className="col-span-1 border-t py-4">
-                      {dateToMonthDay(new Date(write.date))}
-                    </span>
-                    <span className="col-span-4 border-t py-4">
-                      <span className={cn("hover:underline")}>
-                        {write.title}
+                      <span
+                        className={cn(
+                          "col-span-1 py-4 group-hover/item:text-gray-900 transition-all",
+                          writeIndex === 0 ? "border-t" : ""
+                        )}
+                      >
+                        {writeIndex === 0
+                          ? dateToYear(new Date(write.date))
+                          : ""}
                       </span>
-                    </span>
-                  </li>
-                </Link>
-              ))}
-            </ul>
-          </div>
-        ))}
+                      <span className="col-span-1 border-t py-4">
+                        {dateToMonthDay(new Date(write.date))}
+                      </span>
+                      <span className="col-span-4 border-t py-4">
+                        <span className={cn("hover:underline")}>
+                          {write.title}
+                        </span>
+                      </span>
+                    </li>
+                  </Link>
+                ))}
+              </ul>
+            </div>
+          ))}
       </div>
     </div>
   );
